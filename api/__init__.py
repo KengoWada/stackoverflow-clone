5a76ab0f9cd1d0e7365c50a017b61e5d8d277e38
KengoWada <louiskengo3@gmail.com>
2021-06-07 11:53:20 +0300

    Added models and run migrations

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,19 @@
+from config import Config
 from flask import Flask
+from flask_migrate import Migrate, migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .auth.routes import auth_bp
     app.register_blueprint(auth_bp)
@@ -11,3 +22,5 @@ def create_app():
     app.register_blueprint(question_bp)
 
     return app
+
+from . import models
---
+++ api/models.py
@@ -0,0 +1,51 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(128))
+    reset_password_token = db.Column(db.String(120), default='')
+    questions = db.relationship('Question', backref='author', lazy='dynamic')
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+        return data
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.String(256))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'<Question {self.title}>'
